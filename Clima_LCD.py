7a2018ebf2106ff3d5cef392ab162cd9ea070b1a
Juan Blanc <juanblanc14@gmail.com>
2017-06-20 21:48:04 -0300

    Actualización versión 1.2
    Corrección de la API_KEY

--- Clima_LCD.py
+++ Clima_LCD.py
@@ -6,18 +6,18 @@ from pyowm import OWM
 
 try:
     while True:    
-        #print("Obteniendo data")
-       # display.lcd_display_string("Actualizando",1);
-       # display.lcd_display_string("Informacion",2);
+        print("Obteniendo data")
+        display.lcd_display_string("Actualizando",1);
+        display.lcd_display_string("Informacion",2);
         owm_es = OWM(language='es')
         Pais = ',Arg'
-        API_key = 'APIKEY_Aqui'
+        API_key = 'APIKEY_Aqui' #se consiguen en openweathermap.com, son gratuitas
         owm = OWM(API_key)
         apikey = owm.get_API_key()
-        #print 'Key Utilizada->' + owm.get_API_key()
-       # display.lcd_clear()
+        print 'Key Utilizada->' + owm.get_API_key()
+        display.lcd_clear()
         owm.set_API_key(API_key)
-        observation = owm.weather_at_place('Buenos Aires' + Pais)
+        observation = owm.weather_at_place('Buenos Aires' + Pais) #Colocas la ciudad que queres
         w = observation.get_weather()
         presion = str(w.get_pressure())
         presionseparada = presion.split(':')
@@ -28,10 +28,10 @@ try:
         TempA = temperaturaSeparada[3]
         TempF = TempA.split(',')
         humedad = w.get_humidity()
-        #FechaFormato = str(datetime.datetime.now().date())
-        #Fecha = FechaFormato.split('-')
-       # print TempF[0]
-        #print PresF[0]
+        FechaFormato = str(datetime.datetime.now().date())
+        Fecha = FechaFormato.split('-')
+        #print TempF[0] No se usan
+        #print PresF[0] No se usan
         display.lcd_display_string("Temp"+ TempF[0]+'C'+" Hum " + str(humedad)+ '%', 1)
         display.lcd_display_string('Pres ' + PresF[0] +' hPa',2)
         display.lcd_display_string('Fech  '+Fecha[2]+'-'+Fecha[1]+'-'+Fecha[0],3);
@@ -43,5 +43,5 @@ try:
             
         display.lcd_clear()    
 except KeyboardInterrupt: 
-    print("Vaciando")
+    print("Limpiando pantalla...")
     display.lcd_clear()
